429dfec8adc97ff242394a1d604170eddb0b8ffc
izzy-ellis <izzy.ellis223@gmail.com>
2024-11-01 20:41:11 +0000

    Added edit link
    Added a link to edit the page

--- topic.php
+++ topic.php
@@ -4,6 +4,7 @@
 	require 'functions.php';
 
 	$topic = filter_input(INPUT_GET, 'topic', FILTER_VALIDATE_INT);
+	// This seems to throw an error on $topic = 0, but hey I want string page identifiers
 	if (!$topic) {
 		echo "OH SHIT";
 	}
@@ -22,6 +23,7 @@
 <body>
 	<div>
 		<h1><?= $page_info['name'] ?></h1>
+		<a href="edit_topic.php?topic=<?= $page_info['id'] ?>">Edit page</a>
 		<?php
 		$file = fopen($page_info['file'], "r") or die("OH BALLS");
 		echo fread($file, filesize($page_info['file']));
