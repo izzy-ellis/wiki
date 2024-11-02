f36d9b86a8bf785f95c7a1b314200cd0ec7309f8
izzy-ellis <izzy.ellis223@gmail.com>
2024-11-02 15:44:30 +0000

    added bar.php
    Used to add a header bar to the wiki

---
+++ bar.php
@@ -0,0 +1,11 @@
+<!-- This is the fun top bar on the page -->
+<div>
+	<a href="index.php">Home</a>
+	<form>
+		<label for="search">Search:</label>
+		<input type="text" id="search" name="search">
+		<input type="submit">
+	</form>
+	<a href="create_topic">Create page</a>
+</div>
+<hr>
