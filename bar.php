4e9f75bb16bddfd481b064f94e101135fc59a245
izzy-ellis <izzy.ellis223@gmail.com>
2024-11-03 11:09:07 +0000

    Update bar.php
    Added some notes and changed the input slightly

--- bar.php
+++ bar.php
@@ -1,9 +1,15 @@
 <!-- This is the fun top bar on the page -->
+<!-- W3 How To - Autocomplete -->
+<!--
+	We can get a variable from PHP to JavaScript using JSON like so:
+	<script> function('<\?php echo json_encode($php_variable)?>'); </script>
+-->
+<!-- W3 How To - Hoverable Dropdown --> 
 <div>
 	<a href="index.php">Home</a>
 	<form>
 		<label for="search">Search:</label>
-		<input type="text" id="search" name="search">
+		<input type="search" id="search" name="search">
 		<input type="submit">
 	</form>
 	<a href="create_topic.php">Create page</a>
