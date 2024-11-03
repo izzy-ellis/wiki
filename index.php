5801f69fc14aa50c8b360de57f7e11b2256313c1
izzy-ellis <izzy.ellis223@gmail.com>
2024-11-03 12:42:49 +0000

    Update index.php
    Adding notes

--- index.php
+++ index.php
@@ -14,6 +14,8 @@
 		<?php include 'bar.php'; ?>
 		<h1>Home page</h1>
 
+		<!-- Rudimentary testing suggests that "untrusted" will prevent PHP execution
+			and the code will only be displayed if it is in a code block -->
 		<md-block unstrusted src="link-test.md" >
 		</md-block>
 	</div>
