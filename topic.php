aa1d6c7678fd1a6abdee6312c0a8d359801b905e
izzy-ellis <izzy.ellis223@gmail.com>
2024-11-06 20:10:27 +0000

    Update topic.php
    Making sure topic.php works with the changes I made

--- topic.php
+++ topic.php
@@ -1,33 +1,33 @@
 <?php
 	declare(strict_types=1);
-	require 'database-connection.php';
-	require 'functions.php';
-
+	require 'includes/database-connection.php';
+	require 'includes/functions.php';
+	include 'includes/header.php';
+	
 	// Probably add Markdown rendering
 	// Will markdown alter the way it handles links
 
-	$topic = filter_input(INPUT_GET, 'topic', FILTER_VALIDATE_INT);
+	$topic = filter_input(INPUT_GET, 'topic');
 	// This seems to throw an error on $topic = 0, but hey I want string page identifiers
 	if (!$topic) {
 		echo "OH SHIT";
 	}
 
-	$sql = "SELECT * FROM pages WHERE abbreviation = $topic";
+	$sql = "SELECT * FROM pages WHERE abbreviation = '$topic'";
 	$page_info = pdo($pdo, $sql)->fetch();
 
 	if (!$page_info) {
 		echo "OH FUCK";
 	}
+
+	add_header($page_info['title']);
 ?>
-<html>
-<head>
-	<title>Wiki</title>
-</head>
-<body>
+
 	<div>
 		<h1><?= $page_info['title'] ?></h1>
 		<p><?= $page_info['description'] ?></p>
 		<a href="edit_topic.php?topic=<?= $page_info['id'] ?>">Edit page</a>
+		<!-- Testing suggests that this method of echoing Markdown is not vulnerable, but I should test it some more -->
 		<md-block untrusted src="/pages/<?= $page_info['category'] ?>/<?= $page_info['sub_category'] ?>/<?= $page_info['file_name'] ?>"></md-block>
 	</div>
 </body>
