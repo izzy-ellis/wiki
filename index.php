786627acc083a49a64de37e78fae45c4988e4ee1
izzy-ellis <izzy.ellis223@gmail.com>
2024-11-08 22:25:26 +0000

    Testing things

--- index.php
+++ index.php
@@ -14,10 +14,7 @@
 			$sql = "SELECT * FROM pages";
 			$pages = pdo($pdo, $sql)->fetchAll();
 
-			$sql = "INSERT INTO tag_page_relation (page_id, tag_id) VALUES (1, 1)";
-			pdo($pdo, $sql);
-			echo var_dump($pdo->lastInsertId());
-
+		
 			/*foreach ($pages as $page) {
 				$file_path = "/pages/" . $page['category'] . "/" . $page['sub_category'] . "/" . $page['file_name'];
 				?>
