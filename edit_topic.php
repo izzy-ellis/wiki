6bcbd625f085758d7b6fb5bff6b3edbb93630c41
izzy-ellis <izzy.ellis223@gmail.com>
2024-11-13 21:04:07 +0000

    Making it so that you can edit a page

--- edit_topic.php
+++ edit_topic.php
@@ -1,89 +1,57 @@
 <?php
 	declare(strict_types=1);
-	require 'database-connection.php';
-	require 'functions.php';
+	require 'includes/database-connection.php';
+	require 'includes/functions.php';
+	include 'includes/header.php';
 
 	// Move the HTML to be there regardless of the POST?
 
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		// pg 487
 		// This will need updating with the new form
-		$sql = "UPDATE pages SET name = :title WHERE id = :id";
-		$update['id'] = $_POST['id']; // Will this need casting, who knows, not me, an uncertain fisherman
-		$update['title'] = $_POST['title'];
-		pdo($pdo, $sql, $update);
 
-		$file = fopen($_POST['file'], 'w') or die("OH BALLS");
+		var_dump($_POST);
+		$sql = "UPDATE pages SET times_visited = times_visited + 1 WHERE id = {$_POST['id']}";
+
+		$file_path = "pages/" . $_POST['category'] . "/" . $_POST['sub_category'] . "/" . $_POST['file_name'];
+		$file = fopen($file_path, 'w') or die("OH BALLS");
 		fwrite($file, $_POST['text']);
 		fclose($file);
-		?> <a href="topic.php?topic=<?= $_POST['id'] ?>">Return</a> <?php
+		?> <a href="topic.php?topic=<?= $_POST['abbreviation'] ?>">Return</a> <?php
 	} else {
-	$topic = filter_input(INPUT_GET, 'topic', FILTER_VALIDATE_INT);
-	$sql = "SELECT * FROM pages WHERE id = $topic";
+	$topic = filter_input(INPUT_GET, 'topic');
+	$sql = "SELECT title, abbreviation, pages.id AS id, file_name, category.name AS category, sub_category.name AS sub_category FROM pages JOIN category ON category.id = pages.category_id JOIN sub_category ON sub_category.id = pages.sub_category_id WHERE abbreviation = '$topic'";
 	$page_info = pdo($pdo, $sql)->fetch();
 
-	var_dump($_GET);
-	var_dump($_POST);
+	add_header("Edit Topic", ['tooltip.css']);
 ?>
-<html>
-<head>
-	<title>Wiki</title>
-</head>
-<body>
+
 	<h1>Edit page</h1>
 	<form action="edit_topic.php" method="POST">
 		<!-- The full title of the page -->
 		<!-- CSS tooltip on W3 -->
 		<label for="title">Title:</label><br>
-		<input type="text"  id="title" name="title" value="<?= $page_info['name'] ?>"><br>
+		<input type="text"  id="title" name="title" value="<?= $page_info['title'] ?>" readonly><br>
 
 		<!-- This is apparently an insecure way to pass ID --> 
-		<input type="hidden" id="id" name="id" value="<?= $page_info['id'] ?>"><br>
+		<input type="hidden" id="id" name="id" value="<?= $page_info['id'] ?>">
 
 		<!-- This is going to pass the filename over to the POST request -->
-		<input type="hidden" id="file" name="file" value="<?= $page_info['file'] ?>"><br>
+		<input type="hidden" id="file_name" name="file_name" value="<?= $page_info['file_name'] ?>">
 
-		<!-- Drop down menu for the categories -->
-		<!-- These datalists work on the basis that additions can be made
-			 and if empty, will still function -->
-		<input list="category">
-			<datalist id="category">
-				<!-- Get the available categories and dump them here -->
-				<?php 
-				$sql = "SELECT DISTINCT category FROM pages";	// Get the distinct categories
-				$categories = pdo($pdo, $sql)->fetchAll();
-				foreach($categories as $category) {
-					?>
-					<!-- We need to use <option></option> in order to be able to use the "selected" property -->
-					<option value="<?= $category ?>" <?= if ($category == $page_info['category']) { echo "selected";} ?>>
-						<?= $category ?>
-					</option> <?php 
-				}
-				?>
-			</datalist>
+		<!-- This is going to pass the abbreviation over to the POST request -->
+		<input type="hidden" id="abbreviation" name="abbreviation" value="<?= $page_info['abbreviation'] ?>">
 
-		<!-- Drop down menu for sub-categories -->
-		<!-- Ideally we want this to dynamically update when the category is selected but that requires more JavaScript and willpower than I have right now -->
-		<input list="sub_category">
-			<datalist>
-				<!-- Get the available sub-categories and slap them here -->
-				<?php 
-				$sql = "SELECT DISTINCT category, sub_category FROM pages"; // This should get a distinct list of sub-categories
-				$sub_categories = pdo($pdo, $sql)->fetchAll();
-				foreach($csub_categories as $sub_category) {
-					// This is by no means optimal, but it will do the job for now
-					?>
-					<option value="/<?= $sub_category['category'] ?>/<?= $sub_category['sub_category'] ?>">
-						<?= echo $sub_category['category'] . "/" . $sub_category['sub_category'] ?>
-					</option> <?php
-				}
-				?> 
-			</datalist>
+		<!-- for now you don't get to update the category -->
+		<input type="hidden" id="category" name="category" value="<?= $page_info['category'] ?>">
+		<input type="hidden" id="sub_category" name="sub_category" value="<?= $page_info['sub_category'] ?>">
 
 		<!-- Big old text area for the text to go -->
 		<label for="text">Text:</label><br>
-		<textarea id=text name="text"><?php $file = fopen($page_info['file'], 'r') or die("OH BALLS");
-		echo fread($file, filesize($page_info['file']));
+		<textarea id=text name="text"><?php 
+		$file_path = "pages/" . $page_info['category'] . "/" . $page_info['sub_category'] . "/" . $page_info['file_name'];
+		$file = fopen($file_path, 'r') or die("OH BALLS");
+		echo fread($file, filesize($file_path));
 		fclose($file); ?>
 		</textarea><br>
 
