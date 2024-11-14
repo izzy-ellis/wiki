23f0c553cf0a7d422cc7d196dc02c5827b9c1572
izzy-ellis <izzy.ellis223@gmail.com>
2024-11-14 21:52:16 +0000

    Adding recent topics to index page

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	require 'includes/database-connection.php';
 	require 'includes/functions.php';
 	include 'includes/header.php';
+	include 'includes/more_functions.php';
 
 	add_header("Wiki");
 ?>
@@ -18,19 +19,25 @@
 				?><h2><?= $category['name']; ?></h2>
 					<ul>
 						<?php 
-						$sub_category_sql = "SELECT id, name FROM sub_category WHERE parent_id = {$category['id']}";
+						$sub_category_sql = "SELECT id, name, child_count FROM sub_category WHERE parent_id = {$category['id']}";
 						$sub_categories = pdo($pdo, $sub_category_sql)->fetchAll();
 						foreach ($sub_categories as $sub_category) {
-							?> <li><a href=""><?= $sub_category['name'] ?></a></li> <?php
+							?> <li><a href=""><?= $sub_category['name'] ?> (<?= $sub_category['child_count'] ?>)</a></li> <?php
 						} ?> </ul> <?php
 				}
 			?>
 		</div>
 		<div id="main-bar" class="column-two-thirds">
 			<!-- List a number of recent projects here -->
-		</div>
-		
-		
+			<?php
+			$recents_sql = "SELECT title, abbreviation, description, category.name AS category, sub_category.name AS sub_category FROM pages JOIN category ON category.id = pages.category_id JOIN sub_category ON sub_category.id = pages.sub_category_id ORDER BY pages.updated_at DESC LIMIT 3";
+			$recent_pages = pdo($pdo, $recents_sql)->fetchAll();
+
+			foreach($recent_pages as $page) {
+				display_page($page);
+			}
+			?>
+		</div>		
 	</div>
 </body>
 </html>
