5f68bf0cdb2d29f42b5aabe33d268e8644c593d7
izzy-ellis <izzy.ellis223@gmail.com>
2024-11-17 21:41:32 +0000

    Updating index layout

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 ?>
 
 	<div id="content" class="column-full">
-		<div id="side-nav-bar" class="column-third">
+		<div id="side-nav-bar" class="column-fifth">
 			<!-- Here we are going to list all the categories and their sub-categories -->
 			<?php
 				$category_sql = "SELECT id, name FROM category";
@@ -27,7 +27,20 @@
 				}
 			?>
 		</div>
-		<div id="main-bar" class="column-two-thirds">
+		<div class="column-two-fifths">
+			<h1>Popular pages</h1>
+			<?php
+			$popular_sql = "SELECT title, abbreviation, description, category.name AS category, sub_category.name AS sub_category FROM pages JOIN category ON category.id = pages.category_id JOIN sub_category ON sub_category.id = pages.sub_category_id ORDER BY pages.times_visited DESC LIMIT 3";
+
+			$popular_pages = pdo($pdo, $popular_sql)->fetchAll();
+
+			foreach ($popular_pages as $page) {
+				display_page($page);
+			}
+			?>
+		</div>
+		<div id="recents" class="column-two-fifths">
+			<h1>Recent pages</h1>
 			<!-- List a number of recent projects here -->
 			<?php
 			$recents_sql = "SELECT title, abbreviation, description, category.name AS category, sub_category.name AS sub_category FROM pages JOIN category ON category.id = pages.category_id JOIN sub_category ON sub_category.id = pages.sub_category_id ORDER BY pages.updated_at DESC LIMIT 3";
@@ -37,7 +50,7 @@
 				display_page($page);
 			}
 			?>
-		</div>		
+		</div>	
 	</div>
-</body>
-</html>
+
+<?php include "includes/footer.php"; ?>
